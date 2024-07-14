3291fcb5b4ad5f10bf1ae0a621cd586c596e20e1
Santiago Agustin Dopazo <dopazosantiago18@gmail.com>
2024-07-14 16:46:29 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+
+
+def main():
+    while True:
+      sys.stdout.write("$ ")
+      sys.stdout.flush()
+
+      # Wait for user input
+      command = input()
+      print(f"{command}: command not found")
+    
+
+
+if __name__ == "__main__":
+    main()
